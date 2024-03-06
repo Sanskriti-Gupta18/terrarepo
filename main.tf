27a1cc5ba31e3e57578a3a3e85396e0722265500
Sanskriti-Gupta18 <86278463+Sanskriti-Gupta18@users.noreply.github.com>
2024-03-06 09:55:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,16 +7,15 @@ terraform {
   }
 }
 provider "aws" {
-  region     = region_name
-  access_key = access_key
-  secret_key = secret_key
+  region     = var.region_name
+  access_key = var.access_key
+  secret_key = var.secret_key
 }
  
 resource "aws_instance" "webec2" {
   associate_public_ip_address = true
   ami               = "ami-0f403e3180720dd7e"
   instance_type          = "t2.micro"
-  key_name = "tf-key-pair"
   }
   
   variable "access_key" { }
@@ -24,16 +23,3 @@ resource "aws_instance" "webec2" {
   variable "region_name" {
     default = "us-east-1"
   }
-
-resource "aws_key_pair" "tf-key-pair" {
-  key_name   = "tf-key-pair"
-  public_key = tls_private_key.rsa.public_key_openssh
-}
-resource "tls_private_key" "rsa" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-resource "local_file" "tf-key" {
-  content  = tls_private_key.rsa.private_key_pem
-  filename = "tf-key-pair"
-}
